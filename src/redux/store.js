1f233e02a433077968a47326a12e51fee03a425d
aditi <aditi.raghuraj@gmail.com>
2024-07-30 01:09:56 +0530

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "redux";
 import todoReducer from "./todoSlice";
 import authReducer from "./authSlice";
 
-const store = configureStore({
-  reducer: {
-    todos: todoReducer,
-    auth: authReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  todos: todoReducer,
+  auth: authReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
